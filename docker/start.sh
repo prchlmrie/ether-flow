f51766ae46a1f1f7c53b9e60359ec2fc716a3784
Cheilou Puro <puro.cheilou@gmail.com>
2026-06-09 19:02:47 +0800

    Add Render Docker deployment config

---
+++ docker/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+PORT="${PORT:-10000}"
+
+sed -i "s/Listen 80/Listen ${PORT}/" /etc/apache2/ports.conf
+sed -i "s/:80>/:${PORT}>/" /etc/apache2/sites-enabled/000-default.conf
+
+exec apache2-foreground
